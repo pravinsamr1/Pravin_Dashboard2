91053afcdc2966dce9a51bc8e396fef33f4b528a
Pravin <pravinsamr@gmail.com>
2024-10-10 13:45:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const menubtn = document.querySelector('#menu-btn');
+const sideMenu = document.querySelector('aside');
+const closeBtn = document.querySelector('#close-btn');
+const themeToggle = document.querySelector('.theme-toggle')
+
+menubtn.addEventListener('click', ()=>{
+    sideMenu.style.display = 'block';
+})
+closeBtn.addEventListener('click',()=>{
+    sideMenu.style.display = 'none';
+})
+themeToggle.addEventListener('click', ()=>{
+    document.body.classList.toggle('dark-theme-variables');
+})
